66c3f5e1be0657ba975d572dd53aed87b01d17f6
fengl <idage163@163.com>
2015-03-14 21:06:40 +0800

    add

--- animaByIdage/ViewController.m
+++ animaByIdage/ViewController.m
@@ -18,17 +18,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
+    
     //引入文件  用下面的方法控制各个空间的速度
     
     animaView = [[DGAaimaView alloc]initWithFrame:self.view.bounds];
     [self.view addSubview:animaView];
     [animaView DGAaimaView:animaView BigCloudSpeed:1.2 smallCloudSpeed:0.7 earthSepped:0.01 huojianSepped:2.0 littleSpeed:1];
     animaView.animaType=UIClouldAnimaFromUp;
+    
+    UIImageView *headTittleImageView = [[UIImageView alloc]initWithFrame:CGRectMake(50, 30, 220, 60)];
+    headTittleImageView.image = [UIImage imageNamed:@"wenan@3x"];
+    [self.view addSubview:headTittleImageView];
+    
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setFrame:CGRectMake(100, 440, 120, 70)];
+    [button setBackgroundImage:[UIImage imageNamed:@"lijitiyan@3x"] forState:UIControlStateNormal];
+    [self.view addSubview:button];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
